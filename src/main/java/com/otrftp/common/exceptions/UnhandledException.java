7024b86fd9831e4bb78fdf12e32ce5bd9860859f
Yiming Zheng <yimingz89@gmail.com>
2017-08-30 19:03:19 -0400

    Update UnhandledException.java

--- src/main/java/com/otrftp/common/exceptions/UnhandledException.java
+++ src/main/java/com/otrftp/common/exceptions/UnhandledException.java
@@ -2,9 +2,6 @@ package com.otrftp.common.exceptions;
 
 public class UnhandledException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public UnhandledException() {
